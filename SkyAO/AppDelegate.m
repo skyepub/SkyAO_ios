b6dc1d4dd12a235ad2694c8d665e10423d609ef5
skyepub <skytree21@gmail.com>
2024-07-22 21:33:08 +0900

    issue resolved : TTS stops while background mode in ios 17.5

--- SkyAO/AppDelegate.m
+++ SkyAO/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface AppDelegate ()
 
@@ -14,16 +15,47 @@
 @implementation AppDelegate
 @synthesize data;
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    data = [[SkyData alloc]init];
+    data = [[SkyData alloc] init];
+    [self configureAudioSession];
     return YES;
 }
 
+- (void)configureAudioSession {
+    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+    NSError *error = nil;
+    BOOL success = [audioSession setCategory:AVAudioSessionCategoryPlayback
+                                       mode:AVAudioSessionModeSpokenAudio
+                                    options:AVAudioSessionCategoryOptionDuckOthers | AVAudioSessionCategoryOptionAllowBluetooth
+                                      error:&error];
+    if (!success) {
+        NSLog(@"Failed to set audio session category: %@", error);
+    }
+
+    success = [audioSession setActive:YES error:&error];
+    if (!success) {
+        NSLog(@"Failed to activate audio session: %@", error);
+    }
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    NSError *error = nil;
+    BOOL success = [[AVAudioSession sharedInstance] setActive:YES error:&error];
+    if (!success) {
+        NSLog(@"Failed to activate audio session in background: %@", error);
+    }
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    NSError *error = nil;
+    BOOL success = [[AVAudioSession sharedInstance] setActive:YES error:&error];
+    if (!success) {
+        NSLog(@"Failed to activate audio session in foreground: %@", error);
+    }
+}
 
 #pragma mark - UISceneSession lifecycle
-
 
 - (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
     // Called when a new scene session is being created.
@@ -31,12 +63,11 @@
     return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
 }
 
-
 - (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
     // Called when the user discards a scene session.
     // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
     // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
 }
 
-
 @end
+
